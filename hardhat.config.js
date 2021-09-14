01fb4b32adacdf31520f55c37431e5f670b105ea
Sebastian Serrano <sebastian@ripio.com>
2021-09-14 19:46:45 -0300

    etherscan plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require('@openzeppelin/hardhat-upgrades');
 
-const { MAINNET_API_KEY, RINKEBY_API_KEY, MAINNET_PRIVATE_KEY } = process.env;
+const { MAINNET_API_KEY, RINKEBY_API_KEY, MAINNET_PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
 
 
 /**
@@ -19,6 +20,9 @@ module.exports = {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${RINKEBY_API_KEY}`,
       accounts: [`0x${MAINNET_PRIVATE_KEY}`]
     }
+  },
+  etherscan: {
+    apiKey: `${ETHERSCAN_API_KEY}`
   }
 };
 
